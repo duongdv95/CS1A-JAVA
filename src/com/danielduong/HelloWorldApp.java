dea4913097969a5bfc2e48857c42c74fbe65366e
duongdv95 <danielvanduong@gmail.com>
2018-07-02 22:47:36 -0700

    Modularize hello world

--- src/com/danielduong/HelloWorldApp.java
+++ src/com/danielduong/HelloWorldApp.java
@@ -2,6 +2,9 @@ package com.danielduong;
 
 public class HelloWorldApp {
     public static void main(String[] args) {
+        hello();
+    }
+    public static void hello() {
         System.out.println("Hello world");
     }
 }
