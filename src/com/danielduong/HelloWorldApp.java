d25ac9a80b673ad66b9845cdf1510f72c12deabf
duongdv95 <danielvanduong@gmail.com>
2018-07-03 22:48:25 -0700

    Add Hello World with Class

--- src/com/danielduong/HelloWorldApp.java
+++ src/com/danielduong/HelloWorldApp.java
@@ -3,8 +3,16 @@ package com.danielduong;
 public class HelloWorldApp {
     public static void main(String[] args) {
         hello();
+        world spanish = new world();
+        spanish.greetings();
     }
     public static void hello() {
         System.out.println("Hello world");
+    }
+}
+
+class world {
+    public void greetings() {
+        System.out.println("Hola Mundo");
     }
 }
